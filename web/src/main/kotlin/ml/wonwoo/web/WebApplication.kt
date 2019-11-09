3a3717a18aa40b0c25e35bcf763b88680b1ed340
wonwoo <aoruqjfu@gmail.com>
2019-11-09 20:32:04 +0900

    Polish

--- web/src/main/kotlin/ml/wonwoo/web/WebApplication.kt
+++ web/src/main/kotlin/ml/wonwoo/web/WebApplication.kt
@@ -1,9 +1,11 @@
 package ml.wonwoo.web
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class WebApplication
 
 fun main(args: Array<String>) {
